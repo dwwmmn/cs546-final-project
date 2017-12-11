ba4cf98bfd296940fd3f12a659aba7e88f0aad28
Drew Malzahn <drew.malzahn@gmail.com>
2017-12-10 22:57:40 -0500

    Add example decks to test init script

--- scripts/initializeDB.js
+++ scripts/initializeDB.js
@@ -101,6 +101,81 @@ const exampleCards = [
     }
 ];
 
+const exampleDecks = [
+    {
+        _id: "cb7f421e-b965-47cf-b14b-f6bb76cedd99",
+        owner: "e5b01459-2b9b-421d-a69e-58a30ab73043",
+        isPublic: true,
+        name: "Example Deck 1",
+        description: "This is an example deck.",
+        upvotes: [],
+        downvotes: [],
+        cards: [
+            "6a30812d-c25b-458c-a3ff-2df67bd35d2d",
+            "c3f3aa1b-555c-4975-8c19-3a8c24b324c0",
+            "0afff847-4ede-454d-ab41-c094d3ca31d4",
+            "18954f72-4a7f-486a-8738-cf957ae2d904",
+            "56131d89-44a8-46d9-914c-af06a5ab4f58"
+        ],
+    },
+    {
+        _id: "658d7c9a-b293-45ff-82ed-f082ac4ba3ca",
+        owner: "121a31d0-4212-4fd6-979d-5fe38372e90d",
+        isPublic: true,
+        name: "Example Deck 1",
+        description: "This is an example deck.",
+        upvotes: [],
+        downvotes: [],
+        cards: [
+            "6a30812d-c25b-458c-a3ff-2df67bd35d2d",
+            "0afff847-4ede-454d-ab41-c094d3ca31d4",
+            "18954f72-4a7f-486a-8738-cf957ae2d904",
+        ],
+    },
+    {
+        _id: "1b8a8228-7085-494c-bb8e-6b1aa60c88c6",
+        owner: "742eff97-9601-43d0-a0cb-348d926ded62",
+        isPublic: true,
+        name: "Example Deck 1",
+        description: "This is an example deck.",
+        upvotes: [],
+        downvotes: [],
+        cards: [
+            "6a30812d-c25b-458c-a3ff-2df67bd35d2d",
+            "c3f3aa1b-555c-4975-8c19-3a8c24b324c0",
+            "0afff847-4ede-454d-ab41-c094d3ca31d4",
+        ],
+    },
+    {
+        _id: "7e2b74f7-858d-4444-9541-eccd05b42608",
+        owner: "8f9f340c-05f1-4ce8-9239-3dcf7816df79",
+        isPublic: true,
+        name: "Example Deck 1",
+        description: "This is an example deck.",
+        upvotes: [],
+        downvotes: [],
+        cards: [
+            "18954f72-4a7f-486a-8738-cf957ae2d904",
+            "56131d89-44a8-46d9-914c-af06a5ab4f58"
+        ],
+    },
+    {
+        _id: "469e5d2e-9257-4b33-a4e6-3ad88621d665",
+        owner: "3fdf870d-19d3-496c-8f9b-3ce3f7f662f6",
+        isPublic: true,
+        name: "Example Deck 1",
+        description: "This is an example deck.",
+        upvotes: [],
+        downvotes: [],
+        cards: [
+            "6a30812d-c25b-458c-a3ff-2df67bd35d2d",
+            "c3f3aa1b-555c-4975-8c19-3a8c24b324c0",
+            "18954f72-4a7f-486a-8738-cf957ae2d904",
+            "56131d89-44a8-46d9-914c-af06a5ab4f58"
+        ],
+    }
+];
+
 (async () => {
     let users = await collections.users(),
         cards = await collections.cards(),
